45ecca4dab8a3ab8779ad7ae427b9c1b73b67e64
takkyuuplayer <takkyuuplayer@gmail.com>
2014-07-21 21:49:36 +0900

    Show groups

--- app/scripts/eceo/controllers/main.js
+++ app/scripts/eceo/controllers/main.js
@@ -2,9 +2,4 @@
 
 angular.module('eceoApp')
 .controller('MainCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
-  ];
 });
--- app/scripts/eceo/controllers/resource.js
+++ app/scripts/eceo/controllers/resource.js
@@ -1,8 +1,8 @@
 'use strict';
 
 angular.module('eceoApp')
-.controller('ResourceCtrl', function($scope, $http, $location) {
-  $scope.resource = {
+.controller('ResourceCtrl', function($scope, $http, $location, $rootScope) {
+  $rootScope.resource = {
     groups: []
     ,questions: []
     ,answerProgresses: []
@@ -10,18 +10,20 @@ angular.module('eceoApp')
 
   $http.get('/questions/sample.json').success(function(response) {
     var group = {
-      "href": $location.host() + '/questions/sample.json'
+      "host": $location.host()
+      ,"href": $location.host() + "/questions/sample.json"
       ,"group": response.data
     };
-    $scope.resource.groups.push(group);
+    $rootScope.resource.groups.push(group);
 
     _.each(group.group.questions, function(path) {
       $http.get(path).success(function(response) {
         var question = {
-          "href": $location.host() + path
+          "host": $location.host()
+          ,"path": path
           ,"question": response.data
         };
-        $scope.resource.questions.push(question);
+        $rootScope.resource.questions.push(question);
       });
     });
   });
